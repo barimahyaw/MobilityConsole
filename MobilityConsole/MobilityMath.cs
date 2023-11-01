acba0a61da703144516563ba3e088930054f6615
Isaac Nana Gyan Ayeh <nanabarimah22@gmail.com>
2023-11-01 15:49:26 +0000

    Add project files.

---
+++ MobilityConsole/MobilityMath.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MobilityConsole;
+
+public static class MobilityMath
+{
+    public static int Multiply(int num1, int num2)
+    {
+        [DllImport("MobilityMultiplier.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int Multiply(int a, int b);
+        int result = Multiply(num1, num2);
+        return result;
+    }
+}
---
+++ MobilityConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MobilityConsole;
+
+int num1 = 5;
+int num2 = 7;
+var result = MobilityMath.Multiply(num1, num2);
+Console.WriteLine($"Result of {num1} * {num2} = {result}");
+Console.ReadLine();
